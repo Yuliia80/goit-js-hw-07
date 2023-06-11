6c82e941abb8f3518ee75a8dbfb25f3abe34c0d5
Yuliia80 <tretyak_1309@ukr.net>
2023-06-11 21:28:45 +0300

    hw 7 (1)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+  </li>`
+);
+
+galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', 
+  captionPosition: 'bottom', 
+  captionDelay: 250
+});
